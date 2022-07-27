ce1b8ee753d06da778547a6c0808999875ca4de5
Jose Galdamez <josegaldamez1991@gmail.com>
2022-07-27 17:03:45 -0600

    saving

--- src/main/java/main/Uchat.java
+++ src/main/java/main/Uchat.java
@@ -13,15 +13,12 @@ import views.Login;
 public class Uchat {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        
         try {
             FirebaseConnection.connect();
             Login login = new Login();
             login.setVisible(true);
         } catch (IOException e) {
             System.out.println("Algo salió mal durante la conexión.");
-            System.out.println("Hola mundo...");
         }
     }
 }
